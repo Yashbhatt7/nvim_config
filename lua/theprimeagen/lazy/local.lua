d9120166cd76b0e5e21e6de4781cf401cc429bd7
xhunt <yabh678@gmail.com>
2024-10-04 18:23:26 +0530

    modification

--- lua/theprimeagen/lazy/local.lua
+++ lua/theprimeagen/lazy/local.lua
@@ -8,9 +8,9 @@ return {
         vim.keymap.set("n", "<leader>a", mark.add_file)
         vim.keymap.set("n", "<A-e>", ui.toggle_quick_menu)
 
-        vim.keymap.set("n", "<A-h>", function() ui.nav_file(1) end)
-        vim.keymap.set("n", "<A-t>", function() ui.nav_file(2) end)
-        vim.keymap.set("n", "<A-n>", function() ui.nav_file(3) end)
-        vim.keymap.set("n", "<A-s>", function() ui.nav_file(4) end)
+        vim.keymap.set("n", "<A-j>", function() ui.nav_file(1) end)
+        vim.keymap.set("n", "<A-k>", function() ui.nav_file(2) end)
+        vim.keymap.set("n", "<A-l>", function() ui.nav_file(3) end)
+        vim.keymap.set("n", "<A-;>", function() ui.nav_file(4) end)
     end
 }
