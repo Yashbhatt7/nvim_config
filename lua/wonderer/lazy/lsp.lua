1b2ac2a8ae0657e39452ff88c2b911a705ba1cdd
Yashbhatt7 <yabh678@gmail.com>
2025-06-28 08:37:30 +0530

    update

--- lua/wonderer/lazy/lsp.lua
+++ lua/wonderer/lazy/lsp.lua
@@ -1,122 +1,139 @@
 return {
-  "neovim/nvim-lspconfig",
-  dependencies = {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-cmdline",
-    "hrsh7th/nvim-cmp",
-    "L3MON4D3/LuaSnip",
-    "saadparwaiz1/cmp_luasnip",
-    "j-hui/fidget.nvim",
-  },
-  config = function()
-    local cmp = require('cmp')
-    local cmp_lsp = require("cmp_nvim_lsp")
-    local capabilities = vim.tbl_deep_extend(
-      "force",
-      {},
-      vim.lsp.protocol.make_client_capabilities(),
-      cmp_lsp.default_capabilities())
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        "hrsh7th/nvim-cmp",
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
+        "j-hui/fidget.nvim",
+    },
+    config = function()
+        local cmp = require("cmp")
+        local cmp_lsp = require("cmp_nvim_lsp")
+        local capabilities = vim.tbl_deep_extend(
+            "force",
+            {},
+            vim.lsp.protocol.make_client_capabilities(),
+            cmp_lsp.default_capabilities()
+        )
 
-    require("fidget").setup({})
-    require("mason").setup()
-    require("mason-lspconfig").setup({
-      ensure_installed = {
-        "lua_ls",
-        "gopls",
-        "rust_analyzer",
-        "ts_ls",
-        "pylsp",
-        "clangd",
-      },
-      handlers = {
-        -- Default handler for most LSPs
-        function(server_name)
-          local config = {
-            capabilities = capabilities
-          }
-
-          -- Special configuration for clangd
-          if server_name == "clangd" then
-            config.cmd = { "clangd", "--query-driver=/usr/bin/clang++" }
-            config.init_options = {
-              fallbackFlags = { "-std=c++23" },
-            }
-          end
-
-          require("lspconfig")[server_name].setup(config)
-        end,
-
-        -- Special handlers for other LSPs
-        zls = function()
-          local lspconfig = require("lspconfig")
-          lspconfig.zls.setup({
-            root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
-            settings = {
-              zls = {
-                enable_inlay_hints = true,
-                enable_snippets = true,
-                warn_style = true,
-              },
+        require("fidget").setup({})
+        require("mason").setup()
+        require("mason-lspconfig").setup({
+            ensure_installed = {
+                "lua_ls",
+                "gopls",
+                "rust_analyzer",
+                "tsserver",
+                "pylsp",
+                "clangd",
             },
-          })
-          vim.g.zig_fmt_parse_errors = 0
-          vim.g.zig_fmt_autosave = 0
-        end,
+            handlers = {
+                function(server_name)
+                    local config = {
+                        capabilities = capabilities,
+                    }
 
-        ["lua_ls"] = function()
-          local lspconfig = require("lspconfig")
-          lspconfig.lua_ls.setup {
-            capabilities = capabilities,
-            settings = {
-              Lua = {
-                runtime = { version = "Lua 5.1" },
-                diagnostics = {
-                  globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
-                }
-              }
-            }
-          }
-        end,
-      }
-    })
+                    if server_name == "clangd" then
+                        config.cmd = { "clangd", "--clang-tidy", "--query-driver=/usr/bin/clang++" }
+                        config.init_options = {
+                            fallbackFlags = { "-std=c++23" },
+                        }
+                    end
 
-    -- CMP setup remains the same
-    local cmp_select = { behavior = cmp.SelectBehavior.Select }
-    cmp.setup({
-      snippet = {
-        expand = function(args)
-          require('luasnip').lsp_expand(args.body)
-        end,
-      },
-      mapping = cmp.mapping.preset.insert({
-        ['<A-q>'] = cmp.mapping.select_prev_item(cmp_select),
-        ['<A-w>'] = cmp.mapping.select_next_item(cmp_select),
-        ['<A-a>'] = cmp.mapping.confirm({ select = true }),
-        ["<C-Space>"] = cmp.mapping.complete(),
-      }),
-      sources = cmp.config.sources({
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' },
-      }, {
-        { name = 'buffer' },
-      })
-    })
+                    require("lspconfig")[server_name].setup(config)
+                end,
 
-    vim.diagnostic.config({
-      float = {
-        focusable = false,
-        style = "minimal",
-        border = "rounded",
-        source = "always",
-        header = "",
-        prefix = "",
-      },
-    })
-  end
+                ["zls"] = function()
+                    local lspconfig = require("lspconfig")
+                    lspconfig.zls.setup({
+                        root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
+                        settings = {
+                            zls = {
+                                enable_inlay_hints = true,
+                                enable_snippets = true,
+                                warn_style = true,
+                            },
+                        },
+                    })
+                    vim.g.zig_fmt_parse_errors = 0
+                    vim.g.zig_fmt_autosave = 0
+                end,
+
+                ["lua_ls"] = function()
+                    local lspconfig = require("lspconfig")
+                    lspconfig.lua_ls.setup({
+                        capabilities = capabilities,
+                        settings = {
+                            Lua = {
+                                runtime = { version = "Lua 5.1" },
+                                diagnostics = {
+                                    globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
+                                },
+                            },
+                        },
+                    })
+                end,
+            },
+        })
+
+        -- CMP Setup
+        local cmp_select = { behavior = cmp.SelectBehavior.Select }
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    require("luasnip").lsp_expand(args.body)
+                end,
+            },
+            mapping = cmp.mapping.preset.insert({
+                ["<A-q>"] = cmp.mapping.select_prev_item(cmp_select),
+                ["<A-w>"] = cmp.mapping.select_next_item(cmp_select),
+                ["<A-a>"] = cmp.mapping.confirm({ select = true }),
+                ["<C-Space>"] = cmp.mapping.complete(),
+            }),
+            sources = cmp.config.sources({
+                { name = "nvim_lsp" },
+                { name = "luasnip" },
+            }, {
+                { name = "buffer" },
+            }),
+        })
+
+        -- Diagnostic UI
+        vim.diagnostic.config({
+            virtual_text = true,
+            signs = true,
+            underline = true,
+            update_in_insert = false,
+            severity_sort = true,
+            float = {
+                focusable = false,
+                style = "minimal",
+                border = "rounded",
+                source = "always",
+                header = "",
+                prefix = "",
+            },
+        })
+
+        -- CursorHold Popup
+        vim.o.updatetime = 1000
+        vim.api.nvim_create_autocmd("CursorHold", {
+            callback = function()
+                vim.diagnostic.open_float(nil, {
+                    focusable = false,
+                    border = "rounded",
+                    source = "always",
+                    header = "",
+                    prefix = "",
+                })
+            end,
+        })
+    end,
 }
-
 
